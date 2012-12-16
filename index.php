db233652891d2b8bcec16803dfaedc80a05a82e0
J Shamsul Bahri <jibone@gmail.com>
2012-12-17 00:02:03 +0800

    change some styles

--- index.php
+++ index.php
@@ -43,20 +43,48 @@
           <div class="logo-container">
             <a href=""><span class="bold">jS</span>hamsul.com</a>
           </div>
-          <div class="navigation">
-            <a href="">Navigation <i class="icon-down-open-mini"></i></a>
+          <div class="navigation dropdown">
+            <a href="#" class="dropdown-toggle" id="navi-drop" role="button" data-toggle="dropdown">Navigation <i class="icon-down-open-mini"></i></a>
+            <ul id="navi-menu" class="dropdown-menu" role="menu" ara-labelledby="navi-drop">
+              <li><a href="">About</a></li>
+              <li><a href="">Projects</a></li>
+              <li><a href="">Contact</a></li>
+            </ul>
           </div>
         </div>
       </div><!-- navigation container ends -->
       
       <!-- content container start -->
       <div class="content-container">
-      
+        <!-- info container start -->
+        <div class="info-container">
+          <div class="section">
+            <div class="profile-pic">
+              <img src="<?php bloginfo('stylesheet_directory'); ?>/img/profile-pic.jpg">
+            </div>
+            <div class="blog-description">
+              <p>This is the web home of J.Shamsul (@jibone), a web
+              developer based in Kuala Lumpur, Malaysia. He maintains
+              and writes on Jiboneus.com and Wirawanweb. Right now working
+              at EzyPzy.</p>
+            </div>
+          </div>
+        </div><!-- info container ends -->
+        <!-- loop container start -->
+        <div class="loop-container">
+          <div class="post-container">
+            <div class="post-header">
+              <div class="post-date">Monday, 23rd December 2012</div>
+              <h1><a href="">This is the post title</a></h1>
+            </div>
+            <div class="post-body"></div>
+          </div>
+        </div><!-- loop container ends -->
       </div><!-- content container ends -->
       
       <!-- sidebar container start -->
       <div class="sidebar-container">
-
+      
       </div><!-- sidebar container ends -->
     </div><!-- page container ends -->
     
